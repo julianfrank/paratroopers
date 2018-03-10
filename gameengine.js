5590882e4c1fe9338320caec10f9217b5f733655
jf laptop ubuntu <jfempire@yahoo.co.uk>
2018-03-11 00:47:27 +0530

    making sense

--- gameengine.js
+++ gameengine.js
@@ -1,3 +1,5 @@
+
+
 function startStuff() {
 
     var container, stats;
@@ -8,45 +10,50 @@ function startStuff() {
     init();
     animate();
     function init() {
+        //Base container div
         container = document.createElement('div');
         document.body.appendChild(container);
-        var info = document.createElement('div');
-        info.style.position = 'absolute';
-        info.style.top = '10px';
-        info.style.width = '100%';
-        info.style.textAlign = 'center';
-        info.innerHTML = '<a href="http://threejs.org" target="_blank" rel="noopener">three.js</a> webgl - interactive cubes';
-        container.appendChild(info);
+        //Camera Setup
         var aspect = window.innerWidth / window.innerHeight;
-        camera = new THREE.OrthographicCamera(frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, 1, 1000);
+        camera = new THREE.OrthographicCamera(
+            frustumSize * aspect / - 2, frustumSize * aspect / 2,
+            frustumSize / 2, frustumSize / - 2,
+            1, frustumSize)
+        //Scene Setup
         scene = new THREE.Scene();
         scene.background = new THREE.Color(0xf0f0f0);
+        //Light Setup
         var light = new THREE.DirectionalLight(0xffffff, 1);
         light.position.set(1, 1, 1).normalize();
         scene.add(light);
+        //Add Boxes
         var geometry = new THREE.BoxBufferGeometry(44, 44, 44);
         for (var i = 0; i < 4; i++) {
             var object = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({ color: Math.random() * 0xffffff }));
-            object.position.x = Math.random() * 800 - 400;
-            object.position.y = Math.random() * 800 - 400;
-            object.position.z = Math.random() * 800 - 400;
-            object.rotation.x = Math.random() * 2 * Math.PI;
-            object.rotation.y = Math.random() * 2 * Math.PI;
-            object.rotation.z = Math.random() * 2 * Math.PI;
-            object.scale.x = Math.random() 
-            object.scale.y = Math.random() 
-            object.scale.z = Math.random()
+            object.position.x = Math.random() * radius - radius/2
+            object.position.y = Math.random() * radius - radius/2
+            object.position.z = Math.random() * radius - radius/2
+            object.rotation.x = Math.random() * 2 * Math.PI
+            object.rotation.y = Math.random() * 2 * Math.PI
+            object.rotation.z = Math.random() * 2 * Math.PI
+            object.scale.x = 1
+            object.scale.y = 1
+            object.scale.z = 1
             scene.add(object);
         }
+        //Setup Raycaster to monitor mouse movement and events
         raycaster = new THREE.Raycaster();
+        //Setup renderer
         renderer = new THREE.WebGLRenderer();
         renderer.setPixelRatio(window.devicePixelRatio);
         renderer.setSize(window.innerWidth, window.innerHeight);
         container.appendChild(renderer.domElement);
+        //Setup Stats box
         stats = new Stats();
         container.appendChild(stats.dom);
+        //Mouse move event Manager binding
         document.addEventListener('mousemove', onDocumentMouseMove, false);
-        //
+        //Window resize event binding
         window.addEventListener('resize', onWindowResize, false);
     }
     function onWindowResize() {
