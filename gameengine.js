5f4ef05a3f5abb173cd83cab91aee3380ab7706a
jf laptop ubuntu <jfempire@yahoo.co.uk>
2018-04-20 23:15:28 +0530

    bullet also not working :(

--- gameengine.js
+++ gameengine.js
@@ -67,7 +67,8 @@ class GameEngine {
     }
 
     initScene() {
-        let scene = new THREE.Scene({ castShadow: true })
+        //let scene = new THREE.Scene({ castShadow: true })
+        let scene = new Physijs.Scene
         scene.background = new THREE.Color(this.background)
         //scene.fog=new THREE.Fog(0x888888,this.depth-25,this.depth)
         //scene.fog = new THREE.FogExp2(0xffffff, 0.0007)
@@ -255,6 +256,7 @@ class Stickman {
         })
 
         var geometry = new THREE.CylinderGeometry(
+        //var geometry = new Physijs.CylinderGeometry(
             5,                       // radiusTop
             5,                       // radiusBottom
             100,           // height
@@ -263,7 +265,9 @@ class Stickman {
             false                     // openEnded
         );
 
-        this.mesh = new THREE.SkinnedMesh(geometry, material)
+        //this.mesh = new THREE.SkinnedMesh(geometry, material)
+        this.mesh = new Physijs.SkinnedMesh(geometry, material)
+
         this.mesh.add(this.skeleton.bones[0])
         this.mesh.bind(this.skeleton)
 
@@ -380,31 +384,16 @@ class TryBones {
     }
 
     initPhysics() {
-        //Physics testing in progress
-        this.world = new CANNON.World();
-        this.world.gravity.set(0, -9.82, 0);
-        this.world.broadphase = new CANNON.NaiveBroadphase()
 
-        var mass = 5, radius = 10;
-        var sphereShape = new CANNON.Sphere(radius); // Step 1
-        this.sphereBody = new CANNON.Body({ mass: mass, shape: sphereShape }); // Step 2
-        this.sphereBody.position.set(this.bones[0].position.x, this.bones[0].position.y, this.bones[0].position.z);
-        this.sphereBody.angularVelocity.set(0,0,0);
-        this.world.add(this.sphereBody); // Step 3
 
-        var groundShape = new CANNON.Plane();
-        console.log(this.bones[0].quaternion,this.sphereBody)
-        var groundBody = new CANNON.Body({ mass: 0, shape: groundShape,quaternion:{x:this.bones[0].quaternion.x,y:this.bones[0].quaternion.y,z:this.bones[0].quaternion.z,w:this.bones[0].quaternion.w} });
-        groundBody.position.set(0, 0, 0)
-        this.world.add(groundBody);
     }
 
     update() {
         this.bones[3].rotation.z = this.x.tick()
-        this.world.step(this.timeStep)
+        //this.world.step(this.timeStep)
         //console.log(this.sphereBody.position)
         //this.bones[0].position.y = this.sphereBody.position.y
-        this.bones[0].position.copy(this.sphereBody.position)
+        //this.bones[0].position.copy(this.sphereBody.position)
 
     }
 }
