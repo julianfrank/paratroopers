ad0718010e62bd052cfb6f4d3aa9851115c056b1
jf5577 <jfempire@yahoo.co.uk>
2018-04-22 16:23:30 +0530

    ready to ts!

--- ts/main.ts
+++ ts/main.ts
@@ -1,7 +1,12 @@
-//import * as THREE from 'three';
 
+declare var Stats:any;
+(function(){var script=document.createElement('script');script.onload=function(){var stats=new Stats();document.body.appendChild(stats.dom);requestAnimationFrame(function loop(){stats.update();requestAnimationFrame(loop)});};script.src='//rawgit.com/mrdoob/stats.js/master/build/stats.min.js';document.head.appendChild(script);})()
+
+//import * as THREE from 'three';
+declare var THREE:any;
 console.log("main")
 
-const scene = new THREE.Scene();
+
+const scene = new THREE.Scene()
 
 console.log(scene,"cool")
